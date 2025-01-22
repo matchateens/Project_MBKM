51333c1c85d8e3cafafce84a8ea75d889c43a90c
Fateen <39091723+matchateens@users.noreply.github.com>
2025-01-22 23:11:33 +0700

    Update Kakao_Morotai.py

--- Kakao_Morotai.py
+++ Kakao_Morotai.py
@@ -11,7 +11,7 @@ import matplotlib.ticker as ticker  # Import ticker untuk FixedLocator
 # Fungsi untuk memuat data
 @st.cache_data
 def load_data():
-    csv_path = 'D:/Anggi/data/data_kakao.csv'
+    csv_path = '/mount/src/project_mbkm/data_kakao.csv'
     data = pd.read_csv(csv_path)
     return data
 
