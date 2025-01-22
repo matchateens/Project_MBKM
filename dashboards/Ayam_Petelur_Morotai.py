d58d696c282ecc0815cd528dcb6162893f3aab3e
Fateen <39091723+matchateens@users.noreply.github.com>
2025-01-23 02:15:10 +0700

    Update Ayam_Petelur_Morotai.py

--- dashboards/Ayam_Petelur_Morotai.py
+++ dashboards/Ayam_Petelur_Morotai.py
@@ -1,7 +1,12 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+import plotly.express as px
+import graphviz
+import matplotlib.ticker as ticker
 
 # Fungsi untuk memuat data
 def load_data(data_path):
